772c354fdab2648b5080c78ea44b51cf9f9a2028
Farabi9 <ifatkhan633@gmail.com>
2022-12-15 00:06:18 +0600

    banking section

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,17 @@
 document.getElementById('deposite-btn').addEventListener('click', function(){
-    const depositeInput = document.getElementById('deposite-input').value;
-    document.getElementById('deposite-total').innerText = depositeInput;
+    const depositeAmmount = document.getElementById('deposite-total');
+    const amountValue = depositeAmmount.innerText;
+
+    const depositeInput = document.getElementById('deposite-input');
+    const depositeValue = depositeInput.value;
+    
+   depositeAmmount.innerText = parseFloat(amountValue) + parseFloat(depositeValue)
+
+   const balance = document.getElementById('balance-total');
+   const balanceTotal = balance.innerText;
+
+    balance.innerText = parseFloat(balanceTotal) + parseFloat(amountValue) + parseFloat(depositeValue);
+
+
+    depositeInput.value ='';
 })

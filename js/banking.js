bd18d3890f812255299f43852cf191c21b5a76b0
Farabi9 <ifatkhan633@gmail.com>
2023-01-09 02:12:36 +0600

    update balance sheet

--- js/banking.js
+++ js/banking.js
@@ -17,55 +17,45 @@ function textValue(num, inputId){
     ammount.innerText= amouNum + num;
 }
 
+function updateBalance(num, inputId){
+    
+   const balance = document.getElementById(inputId);
+   const balanceTotal = balance.innerText;
+   const bal = parseFloat(balanceTotal)
+
+    balance.innerText = bal + num;
+}
+
 
 document.getElementById('deposite-btn').addEventListener('click', function(){
-    // const depositeAmmount = document.getElementById('deposite-total');
-    // const amountValue = depositeAmmount.innerText;
-    // const amouNum = parseFloat(amountValue);
 
-    // const depositeInput = document.getElementById('deposite-input');
-    // const depositeValue = depositeInput.value;
-    // const depoNum = parseFloat(depositeValue);
     const depoNum = inputValue('deposite-input');
-    
-//   const totalDepo= amouNum + depoNum;
-//   depositeAmmount.innerText =totalDepo;
-textValue(depoNum, 'deposite-total')
+   
+    textValue(depoNum, 'deposite-total')
 
-   const balance = document.getElementById('balance-total');
-   const balanceTotal = parseFloat(balance.innerText);
-//    const balaNum = parseFloat(balanceTotal);
+//    const balance = document.getElementById('balance-total');
+//    const balanceTotal = balance.innerText;
+//    const bal = parseFloat(balanceTotal)
 
-    balance.innerText = balanceTotal + depoNum;
+//     balance.innerText = bal + depoNum;
+updateBalance(depoNum, 'balance-total')
 
-
-    // depositeInput.value ='';
 })
 
 // Module 27-4
 
 document.getElementById('withdraw-btn').addEventListener('click', function(){
-//    const withdrawAmmount = document.getElementById('withdraw-total');
-//    const withdrawValue = withdrawAmmount.innerText;
-//    const withNum = parseFloat(withdrawValue)
-
-//    const withdrawInput = document.getElementById('withdraw-input')
-//    const inputValue = withdrawInput.value;
-//    const inpNum = parseFloat(inputValue)
 
    const inpNum = inputValue('withdraw-input')
 
-//    withdrawAmmount.innerText = withNum + inpNum;
+   textValue(inpNum, 'withdraw-total')
 
-textValue(inpNum, 'withdraw-total')
-
-   const balanceTotal = document.getElementById('balance-total');
-   const totalNum = balanceTotal.innerText;
+//    const balanceTotal = document.getElementById('balance-total');
+//    const totalNum = balanceTotal.innerText;
   
-   const value = parseFloat(totalNum);
-   balanceTotal.innerText = value - inpNum;
-
-//    withdrawInput.value ='';
+//    const value = parseFloat(totalNum);
+//    balanceTotal.innerText = value - inpNum;
+updateBalance(inpNum, 'balance-total')
     })
 
    
